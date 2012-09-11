bf6e29318198c6e777b8c14fcd56e247297675ea
Stephen Wrathall <stevietw@gmail.com>
2012-09-11 21:59:28 +0800

    refactored name of app, and added support for IDE debugging

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '06de395fab737a4baa52f70b9f910179aaa475a48cf8c946d628b6efc135d0ecd5bfddc1291403508c9124e05e491d05c528811eed5f69301ed73dc510b503ab'
+PriceTracker::Application.config.secret_token = '06de395fab737a4baa52f70b9f910179aaa475a48cf8c946d628b6efc135d0ecd5bfddc1291403508c9124e05e491d05c528811eed5f69301ed73dc510b503ab'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-    let(:heading) { 'Sample App' }
+    let(:heading) { 'Price Tracker' }
     let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
@@ -104,7 +104,7 @@ describe "Static pages" do
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
-    click_link "sample app"
+    click_link "price tracker"
     page.should have_selector 'title', text: full_title('')
   end
   
