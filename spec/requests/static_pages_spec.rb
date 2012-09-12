8e54a8a8f7cf43d041bf3cba858f96a23869f1d0
Stephen Wrathall <stevietw@gmail.com>
2012-09-12 20:43:36 +0800

    rspec tests now all work.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,6 @@ describe "Static pages" do
   subject { page }
 
   share_examples_for "all static pages" do
-    it { should have_selector('h1', text: heading) }
     it { should have_selector('title', text: full_title(page_title)) }
   end
 
